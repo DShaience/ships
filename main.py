2620bc4a63ef6a7cfe59480f4bb9665b0bb50511
Shay <shay.teaching@gmail.com>
2020-08-28 16:48:03 +0300

    Added several EDA figures

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import numpy as np
-import pickle
-import os
 from helper_functions import load_and_pickle_data
-
+from matplotlib import pyplot as plt
+import seaborn as sns
+from visualization_functions import measurement_distributions
+sns.set(color_codes=True)
 
 if __name__ == '__main__':
     path_port_visits_train = r'data/port_visits_train.csv'
@@ -18,5 +18,14 @@ if __name__ == '__main__':
     df_port_visits_test = load_and_pickle_data(path_port_visits_test)
     df_vessels_to_label = load_and_pickle_data(path_vessels_to_label)
 
-    
+    # adding label
+    df_port_visits_train = pd.merge(df_port_visits_train, df_vessels_label_train, how='left', left_on='ves_id', right_on='vessel_id')
 
+    measurement_distributions(df_port_visits_train.loc[df_port_visits_train['label'] == 0, 'duration_min'],
+                              df_port_visits_train.loc[df_port_visits_train['label'] == 1, 'duration_min'],
+                              s1_label='neg', s2_label='pos', xlabel='duration_min', title="Duration", toShow=False)
+
+    bplot = sns.boxplot(x="type", y="duration_min", data=df_port_visits_train, linewidth=2)
+    bplot.set_yscale("log")
+    bplot.set_title("Duration of stay (log scale) by vessel type")
+    plt.show()
---
+++ visualization_functions.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def measurement_distributions(s1: pd.Series, s2: pd.Series, s1_label: str, s2_label: str, xlabel: str, title: str, toShow: bool = True):
+    bins = 300
+    f, axes = plt.subplots(1, 1, figsize=(15, 7), sharex=True)
+    sns.distplot(s1, color="darkred", ax=axes, bins=bins, label=s1_label, hist_kws=dict(alpha=0.8))
+    sns.distplot(s2, color="navy", ax=axes, bins=bins, label=s2_label, hist_kws=dict(alpha=0.8))
+    plt.xlabel(xlabel)
+    plt.ylabel('count')
+    plt.title(title)
+    plt.legend()
+    if toShow:
+        plt.show()
+    else:
+        plt.close(f)
