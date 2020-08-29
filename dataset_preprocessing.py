8dbdf6d997ee3e42d1d024dbbeacf9bd6d24e385
Shay <shay.teaching@gmail.com>
2020-08-29 19:19:54 +0300

    minor refactoring

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from helper_functions import load_vessels_dataset
 from vessels_classes import Quotes, ProfilesCounter, DatasetAndFeatures
+from typing import Tuple
 
 
-def feature_generation_main():
+def feature_generation_main() -> Tuple[DatasetAndFeatures, DatasetAndFeatures]:
     fun = Quotes('data/quotes.csv')
     fun.print_quote()
     df_port_visits_train, df_vessels_label_train, df_port_visits_test, df_vessels_to_label = load_vessels_dataset()
@@ -21,8 +22,7 @@ def feature_generation_main():
     test_features_dataset = DatasetAndFeatures(df_port_visits_test, profiles_train)
     fun.print_quote()
 
-    # todo: do feature generation for test also. Write datasets to data/ (as pickles)
-    #  then start working on model thank goodness
+    return train_features_dataset, test_features_dataset
 
 
 if __name__ == '__main__':
