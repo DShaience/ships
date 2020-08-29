7287d33cdb3bd1bfad343a4107d550b7c06fe6e7
Shay <shay.teaching@gmail.com>
2020-08-29 23:40:33 +0300

    Finalized model code, ROC for both train and hold-out, as well as writing final test predictions to file

--- helper_functions.py
+++ helper_functions.py
@@ -1,8 +1,13 @@
 from typing import Tuple, List
+
+import numpy as np
 import pandas as pd
 import pickle
 import os
+
+from matplotlib import pyplot as plt
 from pyunpack import Archive
+from sklearn import metrics
 
 
 def load_and_pickle_data(file_path_csv: str, verbose: bool = True) -> pd.DataFrame:
@@ -69,3 +74,25 @@ def load_vessels_dataset() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd
     df_vessels_to_label = load_and_pickle_data(path_vessels_to_label)
 
     return df_port_visits_train, df_vessels_label_train, df_port_visits_test, df_vessels_to_label
+
+
+def plot_ROC(y_train, y_train_pred_proba, y_holdout, y_holdout_pred_proba, title: str):
+    fontsz = 14
+    train_fpr, train_tpr, _ = metrics.roc_curve(np.array(y_train), y_train_pred_proba)
+    holdout_fpr, holdout_tpr, _ = metrics.roc_curve(np.array(y_holdout), y_holdout_pred_proba)
+    plt.figure(figsize=[10, 7])
+    train_auc = metrics.auc(train_fpr, train_tpr)
+    holdout_auc = metrics.auc(holdout_fpr, holdout_tpr)
+    plt.plot(train_fpr, train_tpr, linewidth=4, label='ROC curve Train (area = %0.2f)' % train_auc)
+    plt.plot(holdout_fpr, holdout_tpr, linewidth=4, label='ROC curve Holdout (area = %0.2f)' % holdout_auc)
+
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate', fontsize=fontsz + 2)
+    plt.ylabel('True Positive Rate', fontsize=fontsz + 2)
+    plt.xticks(fontsize=fontsz)
+    plt.yticks(fontsize=fontsz)
+    plt.title(title, fontsize=fontsz + 4)
+    plt.legend(loc="lower right", fontsize=fontsz + 2)
+    plt.show()
--- model.py
+++ model.py
@@ -1,43 +1,19 @@
 import pickle
 import pandas as pd
-from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV, GroupShuffleSplit, GroupKFold
-from typing import List
 import numpy as np
 from dataset_preprocessing import feature_generation_main
 import seaborn as sns
 from datetime import datetime
-from helper_functions import load_vessels_dataset
+from helper_functions import load_vessels_dataset, plot_ROC
 from report_function import cm_and_classification_report, feature_importance_estimate
 from vessels_classes import Quotes
-from matplotlib import pyplot as plt
+
 sns.set(color_codes=True)
-
-
-def plot_ROC(y_train, y_train_pred_proba, y_holdout, y_holdout_pred_proba, title: str):
-    fontsz = 14
-    train_fpr, train_tpr, _ = metrics.roc_curve(np.array(y_train), y_train_pred_proba)
-    holdout_fpr, holdout_tpr, _ = metrics.roc_curve(np.array(y_holdout), y_holdout_pred_proba)
-    plt.figure(figsize=[10, 7])
-    train_auc = metrics.auc(train_fpr, train_tpr)
-    holdout_auc = metrics.auc(holdout_fpr, holdout_tpr)
-    plt.plot(train_fpr, train_tpr, linewidth=4, label='ROC curve Train (area = %0.2f)' % train_auc)
-    plt.plot(holdout_fpr, holdout_tpr, linewidth=4, label='ROC curve Holdout (area = %0.2f)' % holdout_auc)
-
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate', fontsize=fontsz + 2)
-    plt.ylabel('True Positive Rate', fontsize=fontsz + 2)
-    plt.xticks(fontsize=fontsz)
-    plt.yticks(fontsize=fontsz)
-    plt.title(title, fontsize=fontsz + 4)
-    plt.legend(loc="lower right", fontsize=fontsz + 2)
-    plt.show()
 
 
 def get_model_and_tuned_params(model_name: str):
@@ -60,24 +36,24 @@ def get_model_and_tuned_params(model_name: str):
         model = RandomForestClassifier(random_state=90210)
     elif model_name.lower() == 'AdaBoost'.lower():
         model = AdaBoostClassifier(DecisionTreeClassifier())
+        # tuned_parameters = [{"base_estimator__criterion": ["gini"],
+        #                      "base_estimator__splitter": ["best"],
+        #                      'base_estimator__max_depth': [3],
+        #                      'base_estimator__min_samples_leaf': [10],
+        #                      'base_estimator__class_weight': [{0: 1, 1: 1}],
+        #                      "n_estimators": [20, 30],
+        #                      "random_state": [90210],
+        #                      'learning_rate': [0.01, 0.05]
+        #                      }]
         tuned_parameters = [{"base_estimator__criterion": ["gini"],
                              "base_estimator__splitter": ["best"],
                              'base_estimator__max_depth': [3, 4],
-                             'base_estimator__min_samples_leaf': [10, 20],
+                             'base_estimator__min_samples_leaf': [10, 20, 30],
+                             'base_estimator__class_weight': [{0: 1, 1: 1}, {0: 1, 1: 2}],
                              "n_estimators": [20, 30],
                              "random_state": [90210],
-                             'learning_rate': [0.01, 0.1]
+                             'learning_rate': [0.01, 0.05, 0.1]
                              }]
-    # elif model_name.lower() == 'AdaBoost'.lower():
-    #     model = AdaBoostClassifier(DecisionTreeClassifier())
-    #     tuned_parameters = [{"base_estimator__criterion": ["gini"],
-    #                          "base_estimator__splitter": ["best"],
-    #                          'base_estimator__max_depth': [2, 3, 4],
-    #                          'base_estimator__min_samples_leaf': [10],
-    #                          "n_estimators": [10, 15, 20, 30],
-    #                          "random_state": [90210],
-    #                          'learning_rate': [0.001, 0.01, 0.1]
-    #                          }]
     else:
         raise ValueError("Unsupported classifier type. Cowardly aborting")
     return model, tuned_parameters
@@ -108,7 +84,7 @@ if __name__ == '__main__':
     features_col_names = [col for col in list(train_features_with_label) if col not in [col_id, col_label]]
 
     feature_importance_df = feature_importance_estimate(train_features_with_label[features_col_names], train_features_with_label[col_label])
-    # top_important_features = feature_importance_df['Feature'].values[0:20]  # Top 20 most important features
+    # features_col_names = feature_importance_df['Feature'].values[0:20]  # Top 20 most important features
     print(feature_importance_df.to_string())
 
     rs_vessels_sampling = np.random.RandomState(90210)  # random-state for vessels sampling
@@ -122,10 +98,15 @@ if __name__ == '__main__':
     X_train_groups = df_train[col_id].values
     scaler = StandardScaler()
     scaler.fit(X_train)
+    # Scaling
     X_train_scaled = scaler.transform(X_train)
     X_holdout_scaled = scaler.transform(df_holdout[features_col_names])
     y_train = df_train[col_label].values
     y_holdout = df_holdout[col_label].values
+
+    X_test = test_dataset_obj.features_data_set[features_col_names]
+    X_test_scaled = scaler.transform(X_test)
+    X_test_groups = test_dataset_obj.features_data_set[col_id].values
     print("")
 
     # K-Fold, using group-k-fold by patient-id
@@ -148,14 +129,23 @@ if __name__ == '__main__':
 
     # Prediction and voting
     y_pred_train = clf.predict(X_train_scaled)
-    y_pred_proba_train = clf.predict_proba(X_train_scaled)[:,1]
+    y_pred_proba_train = clf.predict_proba(X_train_scaled)[:, 1]
     y_pred_holdout = clf.predict(X_holdout_scaled)
-    y_pred_proba_holdout = clf.predict_proba(X_holdout_scaled)[:,1]
+    y_pred_proba_holdout = clf.predict_proba(X_holdout_scaled)[:, 1]
 
+    fun.print_quote(add_message="Classification Report")
     cm_and_classification_report(y_train, y_pred_train, labels=[0, 1])
     cm_and_classification_report(y_holdout, y_pred_holdout, labels=[0, 1])
 
+    fun.print_quote(add_message="ROC Curve train vs hold-out")
     plot_ROC(y_train, y_pred_proba_train, y_holdout, y_pred_proba_holdout, "ROC Curve train vs holdout")
+
+    fun.print_quote(add_message="Writing final test-set prediction to file")
+    y_pred_test = clf.predict(X_test_scaled)
+    test_pred_df = pd.DataFrame({"vessel_id": X_test_groups, "y_pred_test": y_pred_test})
+    test_pred_df.to_csv('data/vessels_to_label_prediction.csv', index=False)
+
+    fun.print_quote(add_message="That's all folks!")
 
     """
     Appendices
@@ -189,5 +179,67 @@ if __name__ == '__main__':
     6        std_travel_velocity_kph    0.002427
     2       mean_travel_velocity_kph    0.000928
 
+
+    ================================================
+    Appendix B - Classification Report Output
+    ================================================
+    ==================================================
+    Loading train and test feature objects
+        "But where is everybody?", -Enrico Fermi
+    ==================================================
+       
+    Fitting 6 folds for each of 72 candidates, totalling 432 fits
+    [Parallel(n_jobs=-1)]: Using backend LokyBackend with 4 concurrent workers.
+    [Parallel(n_jobs=-1)]: Done  42 tasks      | elapsed:   29.4s
+    [Parallel(n_jobs=-1)]: Done 192 tasks      | elapsed:  2.3min
+    [Parallel(n_jobs=-1)]: Done 432 out of 432 | elapsed:  5.1min finished
+    Time to complete grid-search: 313.176989 seconds
     
+    Best estimator params:
+        Params: {'base_estimator__class_weight': {0: 1, 1: 2}, 'base_estimator__criterion': 'gini', 'base_estimator__max_depth': 4, 'base_estimator__min_samples_leaf': 30, 
+        'base_estimator__splitter': 'best', 'learning_rate': 0.1, 'n_estimators': 30, 'random_state': 90210}
+        Best Score: 0.802162148091047
+    
+    GridSearch mean-test-score: 0.802162148091047
+    GridSearch std-test-score: 0.009511447197941073
+    
+    ================================================================================================================================================
+    Classification Report
+        "No, I am not dead. Because I refuse to believe the afterlife is run by you. The universe is not so badly designed!", -Jean-Luc Picard
+    ================================================================================================================================================
+    
+         t/p      0     1 
+            0 12411   374 
+            1   629  1371 
+    
+                  precision    recall  f1-score   support
+    
+               0      0.952     0.971     0.961     12785
+               1      0.786     0.685     0.732      2000
+    
+        accuracy                          0.932     14785
+       macro avg      0.869     0.828     0.847     14785
+    weighted avg      0.929     0.932     0.930     14785
+    
+    
+         t/p      0     1 
+            0  5241   198 
+            1   302   596 
+    
+                  precision    recall  f1-score   support
+    
+               0      0.946     0.964     0.954      5439
+               1      0.751     0.664     0.704       898
+    
+        accuracy                          0.921      6337
+       macro avg      0.848     0.814     0.829      6337
+    weighted avg      0.918     0.921     0.919      6337
+    
+    
+    
+    ==================================================
+    ROC Curve train vs hold-out
+        "Tea. Earl Grey. Hot.", -Jean-Luc Picard
+    ==================================================
+
     """
